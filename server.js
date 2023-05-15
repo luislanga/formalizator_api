c785b4401c71fa44c5d9313a084a70339486b76c
Luis Langa <luis.langajr96@gmai.com>
2023-05-15 18:33:34 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+require("dotenv/config")
+const express = require('express')
+const cors = require('cors')
+const PORT = 3000
+
+const API_KEY = process.env.API_KEY
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.listen(PORT, () => console.log('running on port ' + PORT))
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: "formalize o seguinte texto: " + req.body.message}],
+            max_tokens: 100,
+        })
+    }
+    try{
+       const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+       const data = await response.json()
+       res.send(data)
+    }catch(error){
+        console.error(error)
+    }
+})
